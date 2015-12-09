14812fc7f4157ca0ffd1e6d10fc54a36fd14c12c
Mohammed Azad <Varsity.azad@gmail.com>
2015-12-09 15:10:19 -0800

    final comit

--- app/models/Listing.java
+++ app/models/Listing.java
@@ -20,6 +20,39 @@ public class Listing extends Model{
 
 	public String time_Stamp;
 
+	public String getId(){
+		return id;
+	}
+	public void setID(String id){
+		this.id = id;
+	}
+	public String getTitle(){
+		return title;
+	}
+	public void setTitle(String title){
+		this.title = title;
+	}
+	public String getContent(){
+		return content;
+	}
+	public void setContent(String content){
+		this.content = content;
+	}
+	public String getPersonal(){
+		return personal;
+	}
+	public void setPersonal(String personal){
+		this.personal = personal;
+	}
+	public String getSubject(){
+		return subject;
+	}
+	public void setSubject(String subject){
+		this.subject = subject;
+	}
+	public String getTimeStamp(){
+		return time_Stamp;
+	}
 	public void setTimeStamp(String timeStamp){
 		this.time_Stamp = timeStamp;
 	}
